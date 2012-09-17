2ea82034abd896a718ec450811ee51768a80fa80
Pablo May <pablojesusmay@hotmail.com>
2012-09-17 18:11:09 -0500

    Seccion productos corregida, favicon agregado y redes sociales agregados

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,23 @@
+# coding: utf-8
 class PagesController < ApplicationController
   def inicio
+    @title = 'Inicio'
   end
 
   def somos
+    @title = 'Quiénes somos'
   end
 
   def productos
+    @title = 'Nuestros productos'
   end
 
   def recetas
+    @title = 'Recetas Bak'
   end
 
   def contacto
   	@message = Message.new
+    @title = 'Contacto'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
+# coding: utf-8
 module ApplicationHelper
+	def title(title)
+		"Bak Carnes y Alimentos en Cancún Q.Roo | #{title}"
+	end
 end
