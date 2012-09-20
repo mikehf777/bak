708a8ac431f0be6cfb3754cbe2b93a27206e68a9
Pablo May <pablojesusmay@hotmail.com>
2012-09-20 02:22:14 -0500

    Version 1.0 Bak

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,11 @@ module PagesHelper
 	def li_tag(content)
 		content_tag(:li, content)
 	end
+
+	def slide(opt = {})
+		image = opt[:image]
+		title = opt[:title]
+		link = opt[:link]
+		image_tag("slideshow/#{image}", title:"<p>#{title}</p><span class='link'>#{link_to('ver mas', link) if link}</span>")
+	end
 end
